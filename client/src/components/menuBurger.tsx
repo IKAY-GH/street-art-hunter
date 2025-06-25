ea2cce1fc1c39a7c42be9383c8495f944a066f70
le-Faucon09 <jerome.wiera@live.fr>
2025-06-25 11:57:31 +0200

    feat: menuFermente

--- client/src/components/menuBurger.tsx
+++ client/src/components/menuBurger.tsx
@@ -1,13 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router";
 import burger from "../assets/icon/burger.svg";
 import "../assets/styles/burger.css";
 
 function MenuBurger() {
   const [menuOuvert, setMenuOuvert] = useState(false);
+  const menuRef = useRef<HTMLElement>(null);
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        btnRef.current &&
+        !btnRef.current.contains(event.target as Node)
+      ) {
+        setMenuOuvert(false);
+        console.log(menuRef.current, btnRef.current);
+      }
+    }
+    if (menuOuvert) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuOuvert]);
+
   return (
     <>
       <button
+        ref={btnRef}
         type="button"
         className="floating-btn"
         onClick={() => setMenuOuvert((prev) => !prev)}
@@ -16,7 +41,7 @@ function MenuBurger() {
         <img src={burger} alt="Ouvrir le menu" />
       </button>
       {menuOuvert && (
-        <nav className="menu-nav">
+        <nav className="menu-nav" ref={menuRef}>
           <div className="lien">
             <Link to="/gallerie">Galleries</Link>
             <Link to="/cartes">Cartes</Link>
