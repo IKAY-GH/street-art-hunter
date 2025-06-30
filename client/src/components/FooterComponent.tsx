a345bd04313071339d3933f5b34acd3ed1b8a3f6
zarzour <ridouane@MBA.local>
2025-06-30 15:57:33 +0200

    feat: add card

--- client/src/components/FooterComponent.tsx
+++ client/src/components/FooterComponent.tsx
@@ -1,24 +1,19 @@
+import { Link } from "react-router";
 import "./FooterComponent.css";
 
-function Footer() {
+export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
     <footer className="footer">
-      <p>© {year} Street Art Hunter – Tous droits réservés</p>
-      <div className="links">
-        <a href="/mentions-legales">Mentions légales</a>
-        <a href="/cgu">CGU</a>
-        <a
-          href="https://www.linkedin.com/in/ridouane-z-0452a2361/" // mettre votre url linkedin
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-      </div>
+      <p className="footer-left">
+        © {year} Street Art Hunter – Tous droits réservés
+      </p>
+      <nav className="footer-links">
+        <Link to="/mentions-legales">Mentions légales</Link>
+        <Link to="/cgu">CGU</Link>
+        <Link to="/equipe">L'équipe</Link>
+      </nav>
     </footer>
   );
 }
-
-export default Footer;
