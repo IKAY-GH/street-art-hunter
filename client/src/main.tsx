d1118abacde8484d1742085c98373b73151cad9b
CAYUELA Annick <annickcayuela.ac@gmail.com>
2025-07-11 17:07:03 +0200

    chore(main): standardize path naming

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,48 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router";
-
-/* ************************************************************************* */
-
-// Import the main app component
 import App from "./App";
-
-// Import the main pages
-
 import CGU from "./pages/Cgu";
 import Chasse from "./pages/Chasse";
 import Equipe from "./pages/Equipe";
 import Erreur from "./pages/Erreur";
 import Instructions from "./pages/Instructions";
 
-// import Carte from "./pages/MapComponent";
 import MentionsLegales from "./pages/MentionsLegales";
-
-// import Accueil from "./pages/accueil";
-//Changement de nom de composé afin de contourner un problème de commit
 import Classement from "./pages/classement";
 
-// Import additional components for new routes
-// Try creating these components in the "pages" folder
+import Accueil from "./pages/accueil";
 import Gallerie from "./pages/gallerie";
 import Info from "./pages/info";
 
-// import About from "./pages/About";
-import Accueil from "./pages/accueil";
-
-// import Contact from "./pages/Contact";
 import Administrateur from "./pages/administrateur";
 
-// import Formulaire from "./pages/formulaire";
-
-/* ************************************************************************* */
-
-// Create router configuration with routes
-// You can add more routes as you build out your app!
 const router = createBrowserRouter([
   {
-    // The root path
-    element: <App />, // Renders the App component for the home page
+    element: <App />,
     children: [
       {
         path: "/",
@@ -87,12 +64,12 @@ const router = createBrowserRouter([
       // },
 
       {
-        path: "/Cgu",
+        path: "/cgu",
         element: <CGU />,
       },
 
       {
-        path: "/Mentions-Legales",
+        path: "/mentions-Legales",
         element: <MentionsLegales />,
       },
 
@@ -106,19 +83,12 @@ const router = createBrowserRouter([
       },
     ],
   },
-
-  // Try adding a new route! For example, "/about" with an About component
 ]);
 
-/* ************************************************************************* */
-
-// Find the root element in the HTML document
 const rootElement = document.getElementById("root");
 if (rootElement == null) {
   throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
 }
-
-// Render the app inside the root element
 createRoot(rootElement).render(
   <StrictMode>
     <RouterProvider router={router} />
