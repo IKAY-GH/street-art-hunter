b6680b5a2ddc07445a02487f34ec6b6634a38722
le-Faucon09 <jerome.wiera@live.fr>
2025-07-11 07:58:32 +0200

    feat: create new folder auth

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -26,8 +26,8 @@ import Accueil from "./pages/accueil";
 // import Contact from "./pages/Contact";
 import Administrateur from "./pages/administrateur";
 
-import Connexion from "./pages/inscription/connexion/connexion";
-import Inscription from "./pages/inscription/connexion/inscription";
+import Connexion from "./pages/Authentification/connexion/connexion";
+import Inscription from "./pages/Authentification/connexion/inscription";
 
 /* ************************************************************************* */
 
