824aeb0f88fafb5da0020af19809dd32b4db20b5
CAYUELA Annick <annickcayuela.ac@gmail.com>
2025-07-28 10:31:42 +0200

    fix: update server router

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import artistActions from "./modules/artist/artistActions";
 import usersActions from "./modules/user/usersActions";
+import { upload } from "./middlewares/multer";
+import artworkActions from "./modules/artwork/artworkActions";
+import discoveredActions from "./modules/discovered/discoveredActions";
+import discoveredRouter from "./modules/discovered/discorveredRepository";
 
 const router = express.Router();
 
@@ -17,6 +21,13 @@ router.post("/api/user", usersActions.hashPassword, usersActions.add);
 router.get("/api/artists", artistActions.browse);
 router.get("/api/artists/:id", artistActions.read);
 router.post("/api/artists", artistActions.add);
+
+router.get("/api/artworks", artworkActions.browse);
+router.get("/api/artworks/:id", artworkActions.read);
+router.post("/api/artworks", artworkActions.add);
+
+router.post("/api/discovered", upload.single("photo"), discoveredActions.add);
+router.use("/discovered", discoveredRouter);
 
 /* ************************************************************************* */
 
